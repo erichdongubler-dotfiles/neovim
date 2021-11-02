26c2b06e031d9fb70cd3c6559b61b55d92f70d66
Erich Gubler <erichdongubler@gmail.com>
2021-11-01 23:42:34 -0400

    fix(lualine): use correct `diagnostics` config

--- init.lua
+++ init.lua
@@ -367,7 +367,7 @@ require('packer').startup(function()
 				sections = {
 					lualine_a = { 'mode' },
 					lualine_b = { 'filename' },
-					lualine_c = { 'diagnostic', sources = nvim_lsp },
+					lualine_c = { 'diagnostics', sources = { 'nvim_lsp' } },
 					lualine_x = { 'encoding', 'fileformat', 'filetype' },
 					lualine_y = { 'progress' },
 					lualine_z = { 'location' }
