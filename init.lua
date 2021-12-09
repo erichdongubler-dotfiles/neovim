4ca2541c16fbe7edbff91b46315001d9426e8dec
Erich Gubler <erichdongubler@gmail.com>
2021-12-09 09:16:39 -0500

    fix(lualine): fix schema of `nvim_lsp` config

--- init.lua
+++ init.lua
@@ -399,7 +399,7 @@ require('packer').startup(function()
 				sections = {
 					lualine_a = { 'mode' },
 					lualine_b = { 'filename' },
-					lualine_c = { 'diagnostics', sources = { 'nvim_lsp' } },
+					lualine_c = { { 'diagnostics', sources = { 'nvim_lsp' } } },
 					lualine_x = { 'encoding', 'fileformat', 'filetype' },
 					lualine_y = { 'progress' },
 					lualine_z = { 'location' }
