1aada17a22932dad26bbe7624691bfa46e9e399c
Erich Gubler <erichdongubler@gmail.com>
2023-04-19 14:22:39 -0400

    feat(statuscol): add `statuscol` plugin

--- init.lua
+++ init.lua
@@ -702,6 +702,28 @@ packer.startup(function()
 		end,
 	})
 
+	use({
+		"luukvbaal/statuscol.nvim",
+		config = function()
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				relculright = true,
+				segments = {
+					{
+						sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+						click = "v:lua.ScSa",
+					},
+					{
+						sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true },
+						click = "v:lua.ScSa",
+					},
+					{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+					{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+				},
+			})
+		end,
+	})
+
 	-- Navigation
 
 	--   Basic keyboard navigation bindings
