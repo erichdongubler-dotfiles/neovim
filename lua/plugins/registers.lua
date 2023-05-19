4d3d0192e36d5a012d1ff8788ccd39ea90203012
Erich Gubler <erichdongubler@gmail.com>
2023-05-19 00:46:41 -0400

    feat: add `neoclip` for better yank history

--- lua/plugins/registers.lua
+++ lua/plugins/registers.lua
@@ -2,4 +2,30 @@ augroup("ErichDonGublerYank", function(au)
 	au("TextYankPost", "*", bind_fuse(vim.highlight.on_yank), { silent = true })
 end)
 
-return {}
+return {
+	{
+		"AckslD/nvim-neoclip.lua",
+		event = "VeryLazy",
+		dependencies = {
+			"telescope.nvim",
+			"which-key.nvim",
+		},
+		opts = {},
+		config = function(_, opts)
+			require("neoclip").setup(opts)
+			require("telescope").load_extension("neoclip")
+			local which_key = require("which-key")
+			which_key.register({
+				['<Leader>"'] = {
+					bind_fuse(vim.cmd["Telescope"], "neoclip"),
+					"Replace unnamed register with fuzzy history selection",
+				},
+				['<Leader><Leader>"'] = {
+					":Telescope neoclip ",
+					"Replace register with fuzzy history selection (prompt)",
+					silent = false,
+				},
+			})
+		end,
+	},
+}
