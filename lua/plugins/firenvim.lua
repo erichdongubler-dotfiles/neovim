b9c59dfc64102df5f0996b577b2820d590ea23b9
Erich Gubler <erichdongubler@gmail.com>
2023-06-27 16:03:00 -0400

    fix(firenvim): never auto-takeover HTML controls

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -6,5 +6,12 @@ return {
 			require("lazy").load({ plugins = "firenvim", wait = true })
 			vim.fn["firenvim#install"](0)
 		end,
+		init = function()
+			vim.g.firenvim_config = {
+				localSettings = {
+					[".*"] = { takeover = "never" },
+				},
+			}
+		end,
 	},
 }
