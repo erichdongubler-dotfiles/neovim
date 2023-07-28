6b26a41881ceaf6416d704c65e168deb619ed6a6
Erich Gubler <erichdongubler@gmail.com>
2023-07-28 11:22:57 -0400

    feat: add session per Git branch

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -6,6 +6,10 @@ return {
 	"dhruvasagar/vim-prosession",
 	dependencies = {
 		"tpope/vim-obsession",
+		"vim-fugitive", -- required for `prosession_per_branch`
 	},
+	init = function()
+		vim.g.prosession_per_branch = 1
+	end,
 }
 -- TODO: fuzzy session selection?
