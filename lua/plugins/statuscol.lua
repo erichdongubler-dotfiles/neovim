01e64c9c323a2dd923e36621feb66763f8d9738d
Erich Gubler <erichdongubler@gmail.com>
2024-02-22 22:09:07 -0500

    fix(statuscol): unbreak `gitsigns`, just use default config.

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -8,18 +8,6 @@ return {
 			local builtin = require("statuscol.builtin")
 			require("statuscol").setup({
 				relculright = true,
-				segments = {
-					{
-						sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
-						click = "v:lua.ScSa",
-					},
-					{
-						sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true },
-						click = "v:lua.ScSa",
-					},
-					{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-					{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-				},
 			})
 		end,
 	},
