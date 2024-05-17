9cfa8a14ac5debbb0da9535e9da7a6483020bf14
Erich Gubler <erichdongubler@gmail.com>
2024-05-17 13:34:10 -0400

    refactor: remove now-default `backspace` config.

--- lua/plugins/indentation.lua
+++ lua/plugins/indentation.lua
@@ -51,7 +51,6 @@ end, { nargs = 0 })
 
 -- TODO: Does this actually work?
 if vim.g.erichdongubler_initted_indent_opts == nil then
-	vim.opt.backspace = "indent,eol,start"
 	vim.opt.shiftwidth = 4
 	vim.opt.softtabstop = 4
 	vim.opt.tabstop = 4
