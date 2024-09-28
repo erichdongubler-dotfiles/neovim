fdf3af4dee82f3199a9c826d2e9e58063953fab2
Erich Gubler <erichdongubler@gmail.com>
2024-09-28 13:49:37 -0400

    WIP: feat(format): set `formatexpr` to be based on `conform`

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -139,6 +139,10 @@ return {
 				rust = { "rustfmt", lsp_format = "fallback" },
 			},
 		},
+		config = function(_, opts)
+			require("conform").setup(opts)
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
 	},
 
 	-- Debugging
