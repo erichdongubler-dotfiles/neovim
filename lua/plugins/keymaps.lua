cc94ff366efbd0b1a3a5fa4f11d701eb5b88275b
Erich Gubler <erichdongubler@gmail.com>
2024-10-03 11:13:05 -0400

    fix(keymaps): fix force-quit bindings

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -4,8 +4,8 @@ if vim.g.neovide then
 end
 
 noremap("n", "<Leader>ve", ":e " .. vim.fn.stdpath("config"), { desc = "Edit Neovim configuration" })
-noremap("n", "<Leader>", vim.cmd["qa!"], { desc = "Quit (force on all windows)" })
-noremap("n", "<Leader>Q", vim.cmd["q!"], { desc = "Quit (force on current window)" })
+noremap("n", "<Leader>", "<cmd>qa!<CR>", { desc = "Quit (force on all windows)" })
+noremap("n", "<Leader>Q", "<cmd>q!<CR>", { desc = "Quit (force on current window)" })
 noremap("n", "<Leader>q", vim.cmd.q, { desc = "Quit" })
 noremap("n", "<Leader>s", vim.cmd.w, { desc = "Write to file" })
 noremap("n", "<Leader>0", vim.cmd.Inspect, { desc = "Inspect current items at cursor (i.e., highlights)" })
