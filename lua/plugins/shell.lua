d0637166b8c7da1e62349a895b95d24adbba6c78
Erich Gubler <erichdongubler@gmail.com>
2025-05-07 22:52:15 -0400

    FIXUP: official suggestions from upstream

--- lua/plugins/shell.lua
+++ lua/plugins/shell.lua
@@ -1,9 +1,14 @@
-vim.o.shell = "nu"
+-- Ripped straight from
+-- <https://github.com/nushell/integrations/blob/6d2e8250cfb80dc83d9a75a785a31402917315f9/nvim/init.lua>.
+
+vim.opt.shell = "nu"
 -- NOTE: Windows specifies `/s /c`, so we can't use the *nixy `-c` default for `shellcmdflag`.
-vim.o.shellcmdflag = "-c"
-vim.o.shellpipe = "o+e>|"
-vim.o.shellredir = "o+e>"
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
+vim.opt.shellcmdflag = "--stdin --no-newline --commands"
+vim.opt.shellpipe = '| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record'
+vim.opt.shellquote = ""
+vim.opt.shellredir = "out+err> %s"
+vim.opt.shelltemp = false
+vim.opt.shellxescape = ""
+vim.opt.shellxquote = ""
 
 return {}
