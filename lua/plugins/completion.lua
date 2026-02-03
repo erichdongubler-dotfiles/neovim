a3967f2736387819462256f335ffa1d65d22bbf0
Erich Gubler <erichdongubler@gmail.com>
2026-02-03 13:25:25 -0500

    style: fix up `completion` module

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -91,11 +91,11 @@ return {
 			sources = {
 				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 				providers = {
-				  lazydev = {
-					name = "LazyDev",
-					module = "lazydev.integrations.blink",
-					score_offset = 100,
-				  },
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					},
 				},
 			},
 		},
